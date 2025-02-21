3ed8cfe114577f53348989e4aaec4d60c3b8acff
Tomáš Suchý <suchyto12@gmail.com>
2025-02-21 12:23:11 +0100

    created basic layout with css and added form inputs and btns with listeners

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,23 +1,28 @@
-const startBtn = document.getElementById('startButton')
-const endBtn = document.getElementById('stopButton')
+//Control buttons
+const startBtn = document.getElementById('startBtn');
+const endBtn = document.getElementById('stopBtn');
+//Form inputs
+const locationIdElement = document.getElementById('locationId');
+const startDateElement = document.getElementById('startDate');
+const endDateElement = document.getElementById('endDate');
 
 startBtn.onclick = () => {
   const prefs = {
     locationId: locationIdElement.value,
     startDate: startDateElement.value,
     endDate: endDateElement.value,
-  }
-  chrome.runtime.sendMessage({ event: 'onStart', prefs })
-}
+  };
+  chrome.runtime.sendMessage({ event: 'onStart', prefs });
+};
 
 endBtn.onclick = () => {
-  chrome.runtime.sendMessage({ event: 'onStop' })
-}
+  chrome.runtime.sendMessage({ event: 'onStop' });
+};
 
-chrome.storage.local.get(['locationid', 'startDate', 'endDate'], (result) => {
+chrome.storage.local.get(['locationId', 'startDate', 'endDate'], (result) => {
   const { locationId, startDate, endDate } = result;
 
-  if (locationId) locationIdElement.value = locationId
-  if (startDate) startDateElement.value = locationId
-  if (endDate) endDateElement.value = locationId
-})
+  if (locationId) locationIdElement.value = locationId;
+  if (startDate) startDateElement.value = startDate;
+  if (endDate) endDateElement.value = endDate;
+});
