86707dcd00919286967bc7a033471159ba450bcb
Tomáš Suchý <suchyto12@gmail.com>
2025-02-25 12:04:41 +0100

    added form validations

--- src/background.js
+++ src/background.js
@@ -21,9 +21,36 @@ chrome.runtime.onMessage.addListener(data => {
 
 const handleOnStop = () => {
   console.log('On stop in bg');
+  setRunningStatus(false);
+  stopAlarm();
 };
 
 const handleOnStart = (prefs) => {
   console.log('On start in bg', 'prefs received:', prefs)
   chrome.storage.local.set(prefs);
+  setRunningStatus(true);
+  createAlarm();
+};
+
+const ALARM_JOB_NAME = "DROP";
+const setRunningStatus = (isRunning) => {
+  chrome.storage.local.set({ isRunning });
+};
+
+const createAlarm = () => {
+  chrome.alarms.get(ALARM_JOB_NAME, existingAlarm => {
+    if (!existingAlarm) {
+      chrome.alarms.create(ALARM_JOB_NAME, {
+        periodInMinutes: 1.0
+      });
+    }
+  })
+};
+
+chrome.alarms.onAlarm.addListener(() => {
+  console.log('OnAlarm scheduled code running');
+});
+
+const stopAlarm = () => {
+  chrome.alarms.clearAll();
 };
--- src/popup/index.js
+++ src/popup/index.js
@@ -1,12 +1,83 @@
 //Control buttons
 const startBtn = document.getElementById('startBtn');
-const endBtn = document.getElementById('stopBtn');
+const stopBtn = document.getElementById('stopBtn');
 //Form inputs
 const locationIdElement = document.getElementById('locationId');
 const startDateElement = document.getElementById('startDate');
 const endDateElement = document.getElementById('endDate');
+//Span listeners
+const runningSpan = document.getElementById('runningSpan');
+const stoppedSpan = document.getElementById('stoppedSpan');
+
+//Error messages
+const locationIdError = document.getElementById('locationIdError');
+const startDateError = document.getElementById('startDateError');
+const endDateError = document.getElementById('endDateError');
+
+const hideElement = (elem) => {
+  elem.style.display = 'none';
+};
+const showElement = (elem) => {
+  elem.style.display = 'block';
+};
+const disableElement = (elem) => {
+  elem.disabled = true;
+};
+const enableElement = (elem) => {
+  elem.disabled = false;
+};
+
+const handleOnStartState = () => {
+  //Tags
+  showElement(runningSpan);
+  hideElement(stoppedSpan);
+  //Control btns
+  disableElement(startBtn);
+  enableElement(stopBtn);
+  //Inputs
+  disableElement(locationIdElement);
+  disableElement(startDateElement);
+  disableElement(endDateElement);
+};
+const handleOnStopState = () => {
+  //Tags
+  showElement(stoppedSpan);
+  hideElement(runningSpan);
+  //Control btns
+  disableElement(stopBtn);
+  enableElement(startBtn);
+  //Inputs
+  enableElement(locationIdElement);
+  enableElement(startDateElement);
+  enableElement(endDateElement);
+};
+
+const performOnStartValidations = () => {
+  if (!locationIdElement.value) {
+    showElement(locationIdError);
+  } else {
+    hideElement(locationIdError);
+  }
+  if (!startDateElement.value) {
+    showElement(startDateError);
+  } else {
+    hideElement(startDateError);
+  }
+  if (!endDateElement.value) {
+    showElement(endDateError);
+  } else {
+    hideElement(endDateError);
+  }
+
+  return locationIdElement.value && startDateElement.value && endDateElement.value;
+}
 
 startBtn.onclick = () => {
+  const allFieldsValid = performOnStartValidations();
+
+  if (!allFieldsValid) return;
+
+  handleOnStartState();
   const prefs = {
     locationId: locationIdElement.value,
     startDate: startDateElement.value,
@@ -16,18 +87,24 @@ startBtn.onclick = () => {
   chrome.runtime.sendMessage({ event: 'onStart', prefs });
 };
 
-endBtn.onclick = () => {
+stopBtn.onclick = () => {
+  handleOnStopState();
   chrome.runtime.sendMessage({ event: 'onStop' });
 };
 
-chrome.storage.local.get(['locationId', 'startDate', 'endDate', 'locations'], (result) => {
-  const { locationId, startDate, endDate, locations } = result;
+chrome.storage.local.get(['locationId', 'startDate', 'endDate', 'locations', 'isRunning'], (result) => {
+  const { locationId, startDate, endDate, locations, isRunning } = result;
 
   setLocations(locations);
 
   if (locationId) locationIdElement.value = locationId;
   if (startDate) startDateElement.value = startDate;
   if (endDate) endDateElement.value = endDate;
+  if (isRunning) {
+    handleOnStartState();
+  } else {
+    handleOnStopState();
+  }
 });
 
 const setLocations = (locations) => {
